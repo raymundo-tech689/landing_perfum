d4e3364a8776e2dbdb48145c559048f65e21f9e8
Raymundo Ramos <ramosraymundo551@gmail.com>
2024-06-11 16:11:20 -0600

    configurations_first

---
+++ main.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Fragancias </title> 
+    <!-- styles -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+    <link href="../landing_perfum/assets/css/styles.css" >
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+<!-- navbar -->
+<body class="p-3 m-0 border-0 bd-example m-0 border-0">
+<nav class="navbar navbar-expand-lg navbar-bg-custom">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;"> Inicio </font></font></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;"> Nuevas fragancias </font></font></a>
+            </li>
+            <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">
+                Categorías
+            </font></font></a>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#"> Hombre </a></li>
+                <li><a class="dropdown-item" href="#"> Mujer </a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="dropdown-item" href="#"> Conocenos</a></li>
+            </ul>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;"> Acerca de nosotros </font></font></a>
+            </li>
+            <!-- <li class="nav-item">
+            <a class="nav-link disabled" aria-disabled="true"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Desactivado</font></font></a>
+            </li> -->
+        </ul>
+        <form class="d-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Buscar">
+            <button class="btn btn-outline-info" type="submit"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Buscar</font></font></button>
+        </form>
+        </div>
+    </div>
+</nav>
+<br>
+
+<!-- carrousel -->
+<div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+<div class="carousel-inner">
+    <div class="carousel-item active" data-bs-interval="10000">
+    <img src="./images/carrousel/perfumeHombre2.jpg" class="rounded mx-auto d-block img-thumbnail" alt="...">
+    </div>
+    <div class="carousel-item" data-bs-interval="2000">
+    <img src="./images/carrousel/perfumeHombre3.jpg" class="rounded mx-auto d-block img-thumbnail" alt="...">
+    </div>
+    <div class="carousel-item">
+    <img src="./images/carrousel/perfumeMujer1.jpg" class="rounded mx-auto d-block img-thumbnail" alt="...">
+    </div>
+    <div class="carousel-item">
+    <img src="./images/carrousel/perfumeHombre5.jpg" class="rounded mx-auto d-block img-thumbnail" alt="...">
+    </div>
+</div>
+<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+</button>
+<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+</button>
+</div>
+
+
+<br>
+<!-- footer -->
+<footer class="py-3 my-4">
+    <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+    <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Inicio</a></li>
+    <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Nuevas fragancias</a></li>
+    <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Categorías</a></li>
+    <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Conocenos</a></li>
+    <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Comunicate con nosotros</a></li>
+    </ul>
+    <p class="text-center text-body-secondary">© 2024 Fragancias Company, Inc</p>
+</footer>
+
+</body>
+
+<!-- scripts -->
+<script src="/docs/5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+</html>
